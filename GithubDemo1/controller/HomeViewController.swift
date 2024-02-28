34e6c83d1966a5431e8c9194f8bf7d6c68636c70
DavidiCode <akbar.icode@gmail.com>
2024-02-28 22:35:18 +0500

    Initial Commit

---
+++ GithubDemo1/controller/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  GithubDemo1
+//
+//  Created by Akbar Ibodulloyev on 28/02/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
